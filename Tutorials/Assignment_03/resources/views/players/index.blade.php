191a9a888149e15ce8e223bd945e0f3dc53db63d
Theinhtike0 <theinhtikemm0@gmail.com>
2022-08-05 02:03:58 +0630

    Assignment_03

---
+++ Tutorials/Assignment_03/resources/views/players/index.blade.php
@@ -0,0 +1,68 @@
+@extends('players.layout')
+ 
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left"><br>
+                <h2>LARAVEL CRUD</h2>
+            </div><br>
+            <div class="col-md-4">
+                <form action="/search" method="get">
+                    <div class="input-group">
+                        <input type="search" name="search" class="form-control"><br><br>
+                        <span class="input-group-prepend">
+                           <button type="submit" class="btn btn-primary">Search</button>
+                        </span>
+                    </div>
+                </form>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('players.create') }}"> Create New Player</a>
+            </div>
+        </div>
+    </div>
+   <br><br>
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+   
+    <table class="table table-bordered">
+        <tr>
+            <th>ID</th>
+            <th>Team_id</th>
+            <th>Player</th>
+            <th>Number</th>
+            <th>Foot</th>
+            <th>Salary</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($players as $player)
+        <tr>
+            <td>{{ $player->id }}</td>
+            <td>{{ $player->Team_id }}</td>
+            <td>{{ $player->Player }}</td>
+            <td>{{ $player->Number }}</td>
+            <td>{{ $player->Foot }}</td>
+            <td>{{ $player->Salary }}</td>
+            <td>
+                <form action="{{ route('players.destroy',$player->id) }}" method="POST">
+                <a class="btn btn-info" href="{{ route('players.show',$player->id) }}">Show</a>
+    
+                <a class="btn btn-primary" href="{{ route('players.edit',$player->id) }}">Edit</a>
+                
+   
+                    @csrf
+                    @method('DELETE')
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+  
+   
+      
+@endsection
---
+++ Tutorials/Assignment_03/resources/views/players/show.blade.php
@@ -0,0 +1,50 @@
+@extends('players.layout')
+  
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Player</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('players.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Team_id:</strong>
+                {{ $player->Team_id }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Player:</strong>
+                {{ $player->Player }}
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Number:</strong>
+                {{ $player->Number }}
+            </div>
+        </div>
+        
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Foot:</strong>
+                {{ $player->Foot }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Salary:</strong>
+                {{ $player->Salary }}
+            </div>
+        </div>
+        
+    </div>
+@endsection
